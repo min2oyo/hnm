c1f10305a3c0ffc70118a3ebdc83304455043d95
min2oyo <min2u@outlook.com>
2022-06-09 19:40:44 +0900

    API 부르기

---
+++ src/component/ProductCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img src='https://mixxo.com/web/product/tiny/202206/74c76e731c994b13ddb9a8894cb9346f.jpg' alt='product' />
+      <div>상품명</div>
+      <div>가격</div>
+      <div>신상?</div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
+
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    let url = `http://localhost:3001/products`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProductList(data);
+  };
+
   return (
     <div>
-
+      <ProductCard />
     </div>
   );
 };

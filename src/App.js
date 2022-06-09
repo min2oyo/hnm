93183ad54e583232257ede3a26c9dcf815ccc368
min2oyo <min2u@outlook.com>
2022-06-09 15:19:52 +0900

    라우터 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+// 1. 전체 상품 페이지, 로그인, 상품 상세 페이지
+// 2. 전체 상품 페이지에서는 전체 상품을 볼 수 있다
+// 3. 로그인 버튼을 누르면 로그인 페이지가 나온다
+//  - 상품 디테일을 눌렀으나 고르인이 안 되어 있으면 로그인 페이지로 이동한다
+// 4. 로그인이 되어 있으면 상품 디테일 페이지를 볼 수 있다
+// 5. 로그아웃 버튼을 클릭하면 로그아웃이 된다
+//  - 로그아웃이 되면 상품 디테일 페이지를 볼 수 없다. 다시 로그인 페이지가 보인다
+// 6. 로그인을 하면 로그아웃이 보이고 로그아웃을 하면 로그인이 보인다
+// 7. 상품을 검색할 수 있다
+
+import { Routes, Route } from "react-router-dom";
 import './App.css';
+import ProductAll from "./page/ProductAll";
+import ProductDetail from "./page/ProductDetail";
+import Login from "./page/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<ProductAll />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }

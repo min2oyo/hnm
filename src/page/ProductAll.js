94ef015b12c5d98a297effd177abaeb5d2ffcc35
min2oyo <min2u@outlook.com>
2022-06-10 11:32:53 +0900

    상품검색 | 필터

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 import ProductCard from '../component/ProductCard';
 
 const ProductAll = () => {
 
   const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
 
   useEffect(() => {
     getProducts();
-  }, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
 
   const getProducts = async () => {
-    let url = `http://localhost:3001/products`;
+    let searchQuery = query.get(`q`) || ``;
+    let url = `http://localhost:3001/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
     setProductList(data);

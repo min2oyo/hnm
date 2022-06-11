cac84b5c21860839cea8a26f07c689866d4966c3
min2oyo <min2u@outlook.com>
2022-06-11 21:20:59 +0900

    combineReducer

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -7,7 +7,7 @@ import { productAction } from '../redux/actions/productAction';
 
 const ProductAll = () => {
 
-  const productList = useSelector(state => state.productList);
+  const productList = useSelector(state => state.product.productList);
   const [query, setQuery] = useSearchParams();
   const dispatch = useDispatch();
 
